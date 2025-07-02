0eb96ace30cf70b7e7a3ec89a739a8aa1bb13f6e
Nkbarbosa7 <barbosa.santos.nickolas@escola.pr.gov.br>
2025-07-02 20:42:56 -0300

    confirmar

---
+++ main.js
@@ -0,0 +1,12 @@
+const numeroSenha = document.querySelector('.parametro-senha_texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhaSenha;
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+const numeros = '0123456789';
+const simbolos = '!@%*?';
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+const campoSenha = document.querySelectorAll('#campo-senha');
+const checkbox = document.querySelectorAll('.checkbox');
+const forcaSenha = document.querySelectorAll('forca');
+
